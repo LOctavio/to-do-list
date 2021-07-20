06aa4dbc8678cdf6c53ed25856d46eb0f270acc8
LOctavio <luis_ram1997@hotmail.com>
2021-07-20 17:32:46 -0500

    Add array for tasks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+const tasksList = [];
+
 class task {
-    constructor(description, completed = false, index) {
+    constructor(description, index, completed = false) {
         this.description = description;
         this.completed = completed;
         this.index = index;
