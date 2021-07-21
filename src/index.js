8ffd30095e40a0055ccb59ce8c6e11bf5229b544
LOctavio <luis_ram1997@hotmail.com>
2021-07-21 12:34:44 -0500

    Sort tasks by index and add checks an buttons

--- src/index.js
+++ src/index.js
@@ -10,11 +10,17 @@ class task {
 
 const print = () => {
     const container = document.querySelector('ul');
+    tasksList.sort((a, b) => { return a.index > b.index ? 1 : -1 } );
     tasksList.forEach(element => {
         const task = document.createElement('li');
+        const check = document.createElement('input');
         const text = document.createElement('span');
+        const button = document.createElement('button');
+        check.type = "checkbox";
         text.textContent = element.description;
+        task.appendChild(check);
         task.appendChild(text);
+        task.appendChild(button);
         container.appendChild(task);
     });
 }
