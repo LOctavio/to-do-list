d6e65b0bd116766502c0d08a7fadb9d37cdd2ff0
LOctavio <luis_ram1997@hotmail.com>
2021-07-23 18:32:47 -0500

    Fix linter errors

--- src/drag-and-drop.js
+++ src/drag-and-drop.js
@@ -6,12 +6,12 @@ export function drag(item) {
 
 export function drop(container, e) {
   const task2 = e.target.parentElement;
-  if(task.id>task2.id) {
+  if (task.id > task2.id) {
     container.insertBefore(task, task2);
   } else {
-    container.insertBefore( task2, task);
+    container.insertBefore(task2, task);
   }
-  
+
   localStorage.setItem(task2.id, `{"description":"${task.firstChild.nextSibling.textContent}","completed":false,"index":${task2.id}}`);
   localStorage.setItem(task.id, `{"description":"${task2.firstChild.nextSibling.textContent}","completed":false,"index":${task.id}}`);
   [task.id, task2.id] = [task2.id, task.id];
