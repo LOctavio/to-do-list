8097afe7d17b5bfa25613631dd424b6f671e6f37
LOctavio <luis_ram1997@hotmail.com>
2021-07-23 18:29:52 -0500

    Fix drop function

--- src/drag-and-drop.js
+++ src/drag-and-drop.js
@@ -6,9 +6,15 @@ export function drag(item) {
 
 export function drop(container, e) {
   const task2 = e.target.parentElement;
-  container.insertBefore(task, task2);
-  localStorage.setItem(task2.id, task.firstChild.nextSibling.textContent);
-  localStorage.setItem(task.id, task2.firstChild.nextSibling.textContent);
+  if(task.id>task2.id) {
+    container.insertBefore(task, task2);
+  } else {
+    container.insertBefore( task2, task);
+  }
+  
+  localStorage.setItem(task2.id, `{"description":"${task.firstChild.nextSibling.textContent}","completed":false,"index":${task2.id}}`);
+  localStorage.setItem(task.id, `{"description":"${task2.firstChild.nextSibling.textContent}","completed":false,"index":${task.id}}`);
+  [task.id, task2.id] = [task2.id, task.id];
 }
 
 export function allowDrop(ev) {
